9a83d172dbd40d53af71c45d7d9001ca0ed5b01e
kkc109 <83759793+kkc109@users.noreply.github.com>
2021-12-13 01:36:03 +0900

    Delete information.js

--- information.js
+++
@@ -1,7 +0,0 @@
-function openclose(element){
-    if(document.getElementById('element').style.display === 'none') {
-        document.getElementById('element').style.display = 'flex';
-    } 
-    else {
-    document.getElementById('element').style.display = 'none';}
-}
