1a083c668149d40a7d46c05b7580ea8c60891461
kkc109 <83759793+kkc109@users.noreply.github.com>
2021-12-13 01:36:16 +0900

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-function openCloseToc() {
-    if(document.getElementById('link').style.display === 'none') {
-        document.getElementById('link').style.display = 'flex';
-    } 
-    else {
-    document.getElementById('link').style.display = 'none';
-    }
- }
